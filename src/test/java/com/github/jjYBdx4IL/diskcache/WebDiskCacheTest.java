bea836f669157c9167cad21f83a00c8904d3fc88
jjYBdx4IL <jjYBdx4IL@github.com>
2016-12-08 20:48:54 +0100

    use AdHocHttpServer for testing

--- src/test/java/com/github/jjYBdx4IL/diskcache/WebDiskCacheTest.java
+++ src/test/java/com/github/jjYBdx4IL/diskcache/WebDiskCacheTest.java
@@ -1,10 +1,13 @@
 package com.github.jjYBdx4IL.diskcache;
 
+import com.github.jjYBdx4IL.test.AdHocHttpServer;
 import java.io.IOException;
+import java.net.URL;
 import org.junit.AfterClass;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,20 +21,31 @@ public class WebDiskCacheTest {
     private static final Logger LOG = LoggerFactory.getLogger(WebDiskCacheTest.class);
     private static final WebDiskCache cache = new WebDiskCache(null, null, true);
 
+    private static AdHocHttpServer server;
+    private static URL url;
+    
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        server = new AdHocHttpServer();
+        url = server.addStaticContent("/", new AdHocHttpServer.StaticResponse("example content"));
+    }
+    
     @AfterClass
-    public static void afterClass() throws IOException {
+    public static void afterClass() throws IOException, Exception {
         cache.close();
+        server.close();
     }
     
     @Test
     public void testWebGet() throws Exception {
-        byte[] a = cache.get("https://www.google.com/");
-        byte[] b = cache.retrieve("https://www.google.com/");
-        byte[] c = cache.get("https://www.google.com/");
+        byte[] a = cache.get(url.toExternalForm());
+        byte[] b = cache.retrieve(url.toExternalForm());
+        byte[] c = cache.get(url.toExternalForm());
         assertNull(a);
         assertNotNull(b);
         assertNotNull(c);
         assertArrayEquals(b, c);
+        assertArrayEquals("example content".getBytes(), c);
     }
 
 }
